5c7f8c2687f20260272b2f489bd09be8712609a2
MilanPrpa <milanprpa25@gmail.com>
2022-03-03 07:36:13 -0700

    App unable to determine if sentence is palindrome. Will revisit  to fix

--- src/utils/palindromeAlgo.js
+++ src/utils/palindromeAlgo.js
@@ -1,7 +1,11 @@
 export function palindromeAlgo(str){
+  // fix algo to accept either a single word or sentence, regex works but app doesn't
+  str.toUpperCase()
+  str.replace(/\s+/g, '')
   let left = 0;
   let right = str.length - 1;
-  if(str.length === 0){
+  
+  if(str.length === 0) {
     return alert('Please enter a word to begin')
  }
 
